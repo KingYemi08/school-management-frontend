ffa087f66bc329a7140540e7c48361687ee86240
Feranmi Akinyele <akinyeleferanmi19@gmail.com>
2025-08-08 12:11:01 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://learncore.onrender.com/api/v1",
     },
   },
   plugins: [react()],
